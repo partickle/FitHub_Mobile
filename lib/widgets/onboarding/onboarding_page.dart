ec1adebb19d22f8901720ca605975da1def1847c
Persifal <md.putilin@yandex.ru>
2024-04-19 21:16:22 +0300

    Refactoring onboarding page widget

--- lib/widgets/onboarding/onboarding_page.dart
+++ lib/widgets/onboarding/onboarding_page.dart
@@ -65,16 +65,18 @@ class OnboardingPage extends StatelessWidget {
             child: SizedBox(
               width: 55,
               height: 55,
-              child: FloatingActionButton(
-                foregroundColor: kTextColor,
-                backgroundColor: kSecBackgroundColor,
-                elevation: 0,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(10),
+                  foregroundColor: kTextColor,
+                  backgroundColor: kSecBackgroundColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(28),
+                  ),
+                ),
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(28),
-                ),
                 child: const Icon(Icons.arrow_back, size: 25),
               ),
             )
