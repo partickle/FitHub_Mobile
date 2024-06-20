fa0b9aed7d14ef0b31c5ee5ca2d522dadd88e18c
Persifal <md.putilin@yandex.ru>
2024-06-20 17:45:21 +0300

    Add course data

---
+++ lib/features/courses/data/models/exersice/exercise_model.freezed.dart
@@ -0,0 +1,319 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'exercise_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Exercise _$ExerciseFromJson(Map<String, dynamic> json) {
+  return _Exercise.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Exercise {
+  int get id => throw _privateConstructorUsedError;
+  int? get workoutId => throw _privateConstructorUsedError;
+  int get count => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  String get imageUrl => throw _privateConstructorUsedError;
+  String get gifUrl => throw _privateConstructorUsedError;
+  bool get isTime => throw _privateConstructorUsedError;
+  bool get isComplete => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ExerciseCopyWith<Exercise> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ExerciseCopyWith<$Res> {
+  factory $ExerciseCopyWith(Exercise value, $Res Function(Exercise) then) =
+      _$ExerciseCopyWithImpl<$Res, Exercise>;
+  @useResult
+  $Res call(
+      {int id,
+      int? workoutId,
+      int count,
+      String title,
+      String imageUrl,
+      String gifUrl,
+      bool isTime,
+      bool isComplete,
+      String description});
+}
+
+/// @nodoc
+class _$ExerciseCopyWithImpl<$Res, $Val extends Exercise>
+    implements $ExerciseCopyWith<$Res> {
+  _$ExerciseCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? workoutId = freezed,
+    Object? count = null,
+    Object? title = null,
+    Object? imageUrl = null,
+    Object? gifUrl = null,
+    Object? isTime = null,
+    Object? isComplete = null,
+    Object? description = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      workoutId: freezed == workoutId
+          ? _value.workoutId
+          : workoutId // ignore: cast_nullable_to_non_nullable
+              as int?,
+      count: null == count
+          ? _value.count
+          : count // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      gifUrl: null == gifUrl
+          ? _value.gifUrl
+          : gifUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      isTime: null == isTime
+          ? _value.isTime
+          : isTime // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isComplete: null == isComplete
+          ? _value.isComplete
+          : isComplete // ignore: cast_nullable_to_non_nullable
+              as bool,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ExerciseImplCopyWith<$Res>
+    implements $ExerciseCopyWith<$Res> {
+  factory _$$ExerciseImplCopyWith(
+          _$ExerciseImpl value, $Res Function(_$ExerciseImpl) then) =
+      __$$ExerciseImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int id,
+      int? workoutId,
+      int count,
+      String title,
+      String imageUrl,
+      String gifUrl,
+      bool isTime,
+      bool isComplete,
+      String description});
+}
+
+/// @nodoc
+class __$$ExerciseImplCopyWithImpl<$Res>
+    extends _$ExerciseCopyWithImpl<$Res, _$ExerciseImpl>
+    implements _$$ExerciseImplCopyWith<$Res> {
+  __$$ExerciseImplCopyWithImpl(
+      _$ExerciseImpl _value, $Res Function(_$ExerciseImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? workoutId = freezed,
+    Object? count = null,
+    Object? title = null,
+    Object? imageUrl = null,
+    Object? gifUrl = null,
+    Object? isTime = null,
+    Object? isComplete = null,
+    Object? description = null,
+  }) {
+    return _then(_$ExerciseImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      workoutId: freezed == workoutId
+          ? _value.workoutId
+          : workoutId // ignore: cast_nullable_to_non_nullable
+              as int?,
+      count: null == count
+          ? _value.count
+          : count // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      gifUrl: null == gifUrl
+          ? _value.gifUrl
+          : gifUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      isTime: null == isTime
+          ? _value.isTime
+          : isTime // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isComplete: null == isComplete
+          ? _value.isComplete
+          : isComplete // ignore: cast_nullable_to_non_nullable
+              as bool,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ExerciseImpl implements _Exercise {
+  _$ExerciseImpl(
+      {required this.id,
+      required this.workoutId,
+      required this.count,
+      required this.title,
+      required this.imageUrl,
+      required this.gifUrl,
+      required this.isTime,
+      required this.isComplete,
+      required this.description});
+
+  factory _$ExerciseImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ExerciseImplFromJson(json);
+
+  @override
+  final int id;
+  @override
+  final int? workoutId;
+  @override
+  final int count;
+  @override
+  final String title;
+  @override
+  final String imageUrl;
+  @override
+  final String gifUrl;
+  @override
+  final bool isTime;
+  @override
+  final bool isComplete;
+  @override
+  final String description;
+
+  @override
+  String toString() {
+    return 'Exercise(id: $id, workoutId: $workoutId, count: $count, title: $title, imageUrl: $imageUrl, gifUrl: $gifUrl, isTime: $isTime, isComplete: $isComplete, description: $description)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ExerciseImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.workoutId, workoutId) ||
+                other.workoutId == workoutId) &&
+            (identical(other.count, count) || other.count == count) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.imageUrl, imageUrl) ||
+                other.imageUrl == imageUrl) &&
+            (identical(other.gifUrl, gifUrl) || other.gifUrl == gifUrl) &&
+            (identical(other.isTime, isTime) || other.isTime == isTime) &&
+            (identical(other.isComplete, isComplete) ||
+                other.isComplete == isComplete) &&
+            (identical(other.description, description) ||
+                other.description == description));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, workoutId, count, title,
+      imageUrl, gifUrl, isTime, isComplete, description);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ExerciseImplCopyWith<_$ExerciseImpl> get copyWith =>
+      __$$ExerciseImplCopyWithImpl<_$ExerciseImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ExerciseImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Exercise implements Exercise {
+  factory _Exercise(
+      {required final int id,
+      required final int? workoutId,
+      required final int count,
+      required final String title,
+      required final String imageUrl,
+      required final String gifUrl,
+      required final bool isTime,
+      required final bool isComplete,
+      required final String description}) = _$ExerciseImpl;
+
+  factory _Exercise.fromJson(Map<String, dynamic> json) =
+      _$ExerciseImpl.fromJson;
+
+  @override
+  int get id;
+  @override
+  int? get workoutId;
+  @override
+  int get count;
+  @override
+  String get title;
+  @override
+  String get imageUrl;
+  @override
+  String get gifUrl;
+  @override
+  bool get isTime;
+  @override
+  bool get isComplete;
+  @override
+  String get description;
+  @override
+  @JsonKey(ignore: true)
+  _$$ExerciseImplCopyWith<_$ExerciseImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
